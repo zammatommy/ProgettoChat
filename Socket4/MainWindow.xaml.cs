882e00ab9f42ae0b203f2e9617b9d8818139c5c0
Tommaso Zammarchi <TOMMASO.ZAMMARCHI@local.ittcesena.it>
2022-04-20 08:55:03 +0200

    Add another comment

--- Socket4/MainWindow.xaml.cs
+++ Socket4/MainWindow.xaml.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net.Sockets;  //
 using System.Net;        //
-using System.Windows.Threading; //
+using System.Windows.Threading; // 
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -22,40 +22,44 @@ namespace Socket4
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
+
+    //Andremo a realizzare una semplice chat peer to peer(No versione client e server, devono solo mandare e ricevere)
+
     {
-        Socket socket = null;
+        Socket socket = null; //Socket= canale di comunicazione
         DispatcherTimer dTimer = null;
         public MainWindow()
         {
-            socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp); //Abbiamo crato la classe socket
-            IPAddress local_address = IPAddress.Any;
-            IPEndPoint local_endpoint = new IPEndPoint(local_address.MapToIPv4(), 65000); //perchè la cosa funzionerà se i firewall sono sbloccati, possibilità di far comunicare due macchine
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp); //Abbiamo creato la classe socket
+            IPAddress local_address = IPAddress.Any; //Ipaddres = Classe per creare il tuo indirizzo ip
+            IPEndPoint local_endpoint = new IPEndPoint(local_address.MapToIPv4(), 65000 ); //perchè la cosa funzionerà se i firewall sono sbloccati, possibilità di far comunicare due macchine(65.000 porta mittente)
             //Socket ha bisogno di due endpoint
 
             socket.Bind(local_endpoint);    //consente di unire la socket al endpoint locale, posso iniziare a trasmettere qualcosa
 
-            dTimer = new DispatcherTimer();
-            dTimer.Tick += new EventHandler(aggiornamento_dTimer);
+            
+            dTimer = new DispatcherTimer(); //per poterlo fare ce una problematica, c'e una programmazione concorrente,abbiamo cosi un thread che ascolta e uno che gestisce l'invio,dispatcherTimer = un thread ascolta costantemente, nel nostro caso abbiamo scelto 250 millisecondi, ma si può mettere il valore che si vuole a seconda dell'esigenze del mittente. 
+            dTimer.Tick += new EventHandler(aggiornamento_dTimer); // Quando passa dTimer.Interval tempo, chiama il metodo aggiornamento_dTimer
             dTimer.Interval = new TimeSpan(0, 0, 0, 0, 250); // Si parla di 250 millisecondi
             dTimer.Start(); //avvia il timer
 
             InitializeComponent();
         }
 
-        private void BtnInvia_Click(object sender, RoutedEventArgs e)
+        private void BtnInvia_Click(object sender, RoutedEventArgs e) //Bottone di invio
         {
-            IPAddress remote_address = IPAddress.Parse(txtIndirizzo.Text);     //Prendo l'ip da wpf
-            IPEndPoint remote_endpoint = new IPEndPoint(remote_address, int.Parse(txtPorta.Text)); //Prendo la porta da wpf
-            Byte[] Messaggio = Encoding.UTF8.GetBytes(txtMessaggio.Text);    //Trasformo il messaggio in Byte
-            socket.SendTo(Messaggio, remote_endpoint);
-            //Metodo per mandare il messaggio
+            IPAddress remote_address = IPAddress.Parse(txtIndirizzo.Text);     //Prendo l'ip da wpf, Destinatario
+            IPEndPoint remote_endpoint = new IPEndPoint(remote_address, int.Parse(txtPorta.Text)); //Prendo la porta da wpf a scelta
+            Byte[] Messaggio = Encoding.UTF8.GetBytes(txtMessaggio.Text);    //Trasformo  il messaggio che ho in un array in Byte
+            socket.SendTo(Messaggio, remote_endpoint);  //Metodo per inviare il messaggio
+
 
         }
 
-        private void aggiornamento_dTimer(object sender, EventArgs e)
+        private void aggiornamento_dTimer(object sender, EventArgs e) //Metodo per ricevere il messaggio
         {
-            int nBytes = 0;  //se maggiore di zero posso leggere qualcosa, sennò esce dal metodo
-            if ((nBytes = socket.Available) > 0)
+            int nBytes = 0;  //se maggiore di zero posso leggere qualcosa, sennò esce dal metodo.
+            if ((nBytes = socket.Available) > 0) // il > 0 non è obbligatorio
             {
                 //Ricezione dei caratteri in Attesa
                 byte[] buffer = new byte[nBytes];
